220d832466fa8a88bb098782d8a688aa0dad83db
gabolinscya <gabolinscya@stu.otc.school.nz>
2022-03-01 10:59:34 +1300

    Put Player function into its own file

--- Monster Encounter Setup.py
+++ Monster Encounter Setup.py
@@ -53,10 +53,36 @@ def monster_spawner():
         ai_spawned = 5
         enter_word = "a"
         nickname = "Bug"
-    monster_encounter(monster_ai, ai_spawned, enter_word, ai_group_hp, nickname,ai_power,attacks)
 
+def monster_encounter():
+    print("You encountered {} {}!".format(enter_word, monster_ai))
+    if ai_spawned == 1:
+        ai_1_hp = ai_group_hp
+        ai_list = [ai_1_hp]
+    elif ai_spawned == 2:
+        ai_1_hp = ai_group_hp
+        ai_2_hp = ai_group_hp
+        ai_list = [ai_1_hp, ai_2_hp]
+    elif ai_spawned == 3:
+        ai_1_hp = ai_group_hp
+        ai_2_hp = ai_group_hp
+        ai_3_hp = ai_group_hp
+        ai_list = [ai_1_hp, ai_2_hp, ai_3_hp]
+    elif ai_spawned == 4:
+        ai_1_hp = ai_group_hp
+        ai_2_hp = ai_group_hp
+        ai_3_hp = ai_group_hp
+        ai_4_hp = ai_group_hp
+        ai_list = [ai_1_hp, ai_2_hp, ai_3_hp, ai_4_hp]
+    elif ai_spawned == 5:
+        ai_1_hp = ai_group_hp
+        ai_2_hp = ai_group_hp
+        ai_3_hp = ai_group_hp
+        ai_4_hp = ai_group_hp
+        ai_5_hp = ai_group_hp
+        ai_list = [ai_1_hp, ai_2_hp, ai_3_hp, ai_4_hp, ai_5_hp]
 
-def chose_attack_ai(attacks, monster_ai, ai_power, damage_done_ai):
+def chose_attack_ai():
     damage_done_ai = 0
     if monster_ai == "boss":
         Hammer_slam = [2 * ai_power, 2.1 * ai_power, 2.2 * ai_power, 2.3 * ai_power, 2.4 * ai_power, 2.5 * ai_power]
@@ -109,96 +135,3 @@ def chose_attack_ai(attacks, monster_ai, ai_power, damage_done_ai):
             else:
                 damage_done_ai = Scratch[0]
 
-
-def monster_encounter(monster_ai, ai_spawned, enter_word, ai_group_hp, nickname,ai_power,attacks):
-    global ai_1_hp
-    global ai_2_hp
-    global ai_3_hp
-    global ai_4_hp
-    global ai_5_hp
-    print("You encountered {} {}!".format(enter_word, monster_ai))
-    if ai_spawned == 1:
-        ai_1_hp = ai_group_hp
-        ai_list = [ai_1_hp]
-    elif ai_spawned == 2:
-        ai_1_hp = ai_group_hp
-        ai_2_hp = ai_group_hp
-        ai_list = [ai_1_hp, ai_2_hp]
-    elif ai_spawned == 3:
-        ai_1_hp = ai_group_hp
-        ai_2_hp = ai_group_hp
-        ai_3_hp = ai_group_hp
-        ai_list = [ai_1_hp, ai_2_hp, ai_3_hp]
-    elif ai_spawned == 4:
-        ai_1_hp = ai_group_hp
-        ai_2_hp = ai_group_hp
-        ai_3_hp = ai_group_hp
-        ai_4_hp = ai_group_hp
-        ai_list = [ai_1_hp, ai_2_hp, ai_3_hp, ai_4_hp]
-    elif ai_spawned == 5:
-        ai_1_hp = ai_group_hp
-        ai_2_hp = ai_group_hp
-        ai_3_hp = ai_group_hp
-        ai_4_hp = ai_group_hp
-        ai_5_hp = ai_group_hp
-        ai_list = [ai_1_hp, ai_2_hp, ai_3_hp, ai_4_hp, ai_5_hp]
-    player_1_turn(ai_list, player_hp, nickname, ai_spawned, ai_1_hp, ai_2_hp, ai_3_hp, ai_4_hp, ai_5_hp, monster_ai)
-
-
-def player_1_turn(ai_list, player_hp, nickname, ai_spawned, ai_1_hp, ai_2_hp, ai_3_hp, ai_4_hp, ai_5_hp, monster_ai):
-    if player_hp > 0:
-        print("Player HP:{}".format(player_hp))
-        fireball = [30, 31, 32, 33, 34, 35]
-        player_1_turn_choice = input("What do you want to do:'Attack','End'")
-        player_1_turn_choice = player_1_turn_choice.strip().lower()
-        if player_1_turn_choice == "attack":
-            if ai_spawned == 1:
-                player_1_attack_choice = input("Which enemy would you like to interact with:{}".format(monster_ai))
-            elif ai_spawned < 1:
-                player_1_attack_choice = input(
-                    "Which numbered enemy would you like to interact with:{}".format(nickname and len(ai_list)))
-            else:
-                print("Please enter a correct input")
-                player_1_turn(ai_list, player_hp, nickname)
-        elif player_1_turn_choice == "end":
-            print("You passed your turn")
-            damage_done_player = 0
-            # pass to next turn
-        else:
-            print("Please enter a correct input")
-            player_1_turn(ai_list, player_hp, nickname)
-        player_1_attack_choice = player_1_attack_choice.strip().lower()
-        player_1 = input("What attack would you like to preform:'fireball'")
-        player_1.strip().lower()
-        damage_done_player = 0
-        if player_1 == "fireball":
-            random.shuffle(fireball)
-            crit_chance = random.randint(1, 2)
-            if crit_chance == 2:
-                damage_done_player = fireball[0] * 2
-                print("You're fireball did critical damage and did {}!".format(damage_done_player))
-            else:
-                damage_done_player = fireball[0]
-                print("You're fireball hit for {}!".format(damage_done_player))
-        else:
-            print("Please enter a correct input")
-            player_1_turn(ai_list, player_hp, nickname, ai_spawned, ai_1_hp, ai_2_hp, ai_3_hp, ai_4_hp, ai_5_hp, monster_ai)
-        if player_1_attack_choice == "1":
-            ai_1_hp = ai_1_hp - damage_done_player
-        elif player_1_attack_choice == "2":
-            ai_2_hp = ai_2_hp - damage_done_player
-        elif player_1_attack_choice == "3":
-            ai_3_hp = ai_3_hp - damage_done_player
-        elif player_1_attack_choice == "4":
-            ai_4_hp = ai_4_hp - damage_done_player
-        elif player_1_attack_choice == "5":
-            ai_5_hp = ai_5_hp - damage_done_player
-        AI_turn(ai_list, player_hp, nickname, ai_1_hp, ai_2_hp, ai_3_hp, ai_4_hp, ai_5_hp)
-    else:
-        print("Lose message")
-
-
-def AI_turn(ai_list, player_hp, nickname, ai_1_hp, ai_2_hp, ai_3_hp, ai_4_hp, ai_5_hp):
-    print("\n")
-    for num in ai_list:
-        print("{}{} HP: {}".format(nickname, len(ai_list), ai_list))
